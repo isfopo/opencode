c584be4c6004660c105d96596d137e3ea17465e3
Isaac Poole <isaacpoolemusic@gmail.com>
2025-12-12 08:48:00 -0500

    adds chrome mcp

--- gen.py
+++ gen.py
@@ -1,59 +1,70 @@
 import json
 from secrets import GITHUB_PAT, BRIGHT_DATA_TOKEN
 
-config ={
-  "$schema": "https://opencode.ai/config.json",
-  "mcp": {
-    "context7": {
-      "url": "https://mcp.context7.com/mcp",
-      "type": "remote",
-      "enabled": True
+config = {
+    "$schema": "https://opencode.ai/config.json",
+    "mcp": {
+        "context7": {
+            "url": "https://mcp.context7.com/mcp",
+            "type": "remote",
+            "enabled": True,
+        },
+        "fetch": {
+            "url": "https://remote.mcpservers.org/fetch/mcp",
+            "type": "remote",
+            "enabled": True,
+        },
+        "sequential-thinking": {
+            "url": "https://remote.mcpservers.org/sequentialthinking/mcp",
+            "type": "remote",
+            "enabled": True,
+        },
+        "deep-wiki": {
+            "url": "https://mcp.deepwiki.com/mcp",
+            "type": "remote",
+            "enabled": True,
+        },
+        "github": {
+            "url": "https://api.githubcopilot.com/mcp/",
+            "type": "remote",
+            "enabled": True,
+            "headers": {"Authorization": f"Bearer {GITHUB_PAT}"},
+        },
+        "playwright": {
+            "type": "local",
+            "command": ["npx", "@playwright/mcp@latest"],
+            "enabled": True,
+        },
+        "mermaid": {
+            "type": "local",
+            "command": ["npx", "-y", "mcp-mermaid"],
+            "enabled": True,
+        },
+        "xcode": {
+            "type": "remote",
+            "url": f"https://mcp.brightdata.com/mcp?token={BRIGHT_DATA_TOKEN}",
+            "enabled": True,
+        },
+        "mobilenext": {
+            "type": "local",
+            "command": ["npx", "-y", "@mobilenext/mobile-mcp@latest"],
+            "enabled": True,
+        },
+        "homebrew": {
+            "type": "local",
+            "command": ["brew", "mcp-server"],
+            "enabled": True,
+        },
+        "chrome-devtools": {
+            "type": "local",
+            "command": ["npx", "-y", "chrome-devtools-mcp@latest"],
+            "enabled": True,
+        },
     },
-    "fetch": {
-      "url": "https://remote.mcpservers.org/fetch/mcp",
-      "type": "remote",
-      "enabled": True
-    },
-    "sequential-thinking": {
-      "url": "https://remote.mcpservers.org/sequentialthinking/mcp",
-      "type": "remote",
-      "enabled": True
-    },
-    "deep-wiki": {
-      "url": "https://mcp.deepwiki.com/mcp",
-      "type": "remote",
-      "enabled": True
-    },
-    "github": {
-      "url": "https://api.githubcopilot.com/mcp/",
-      "type": "remote",
-      "enabled": True,
-      "headers": {
-        "Authorization": f"Bearer {GITHUB_PAT}"
-      }
-    },
-    "playwright": {
-      "type": "local",
-      "command": ["npx", "@playwright/mcp@latest"],
-      "enabled": True,
-    },
-    "mermaid": {
-      "type": "local",
-      "command": ["npx", "-y", "mcp-mermaid"],
-      "enabled": True,
-    },
-    "xcode": {
-      "type": "remote",
-      "url": f"https://mcp.brightdata.com/mcp?token={BRIGHT_DATA_TOKEN}",
-      "enabled": True,
-    },
-    "mobilenext": {
-        "type": "local",
-        "command": ["npx", "-y", "@mobilenext/mobile-mcp@latest"],
-        "enabled": True,
-    }
-  },
 }
 
 with open("opencode.json", "w") as f:
     f.write(json.dumps(config))
+
+print("Config file generated successfully. with the following content:")
+print(json.dumps(config, indent=4))
